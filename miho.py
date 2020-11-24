ce5f743e70f727c86ddd1e3d13ee62697c550fad
finnolin <finnolin@gmail.com>
2020-11-24 23:30:27 +0100

    added support for Yamaha Reface CS

--- miho.py
+++ miho.py
@@ -1,12 +1,21 @@
 import subprocess
 
+#-------DEVICES-------
+#OP-Z
+#OP-1 Midi Device
+#Arturia KeyStep 32
+#reface CS
+
+
+
+#CONNECTIONS:
 
 #OP-Z Out
 subprocess.Popen(["aconnect", "OP-Z", "OP-1 Midi Device"])
-
-#OP-1 Out
+subprocess.Popen(["aconnect", "OP-Z", "reface CS"])
 
 
 #Keystep Out
 subprocess.Popen(["aconnect", "Arturia KeyStep 32", "OP-1 Midi Device"])
 subprocess.Popen(["aconnect", "Arturia KeyStep 32", "OP-Z"])
+subprocess.Popen(["aconnect", "Arturia KeyStep 32", "reface CS"])
