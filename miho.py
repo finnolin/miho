914d58851c938b0710c923d498ff67f00c9775e3
finnolin <finnolin@gmail.com>
2020-11-24 18:04:53 +0000

    set basics for connections

--- miho.py
+++ miho.py
@@ -2,4 +2,5 @@ import subprocess
 
 print ("cecking midi connections")
 
-suprocess.Popen(["aconnect", "-l"])
+subprocess.Popen(["aconnect", "OP-Z", "OP-1 Midi Device"])
+subprocess.Popen(["aconnect", "-l"])
