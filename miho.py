ca857134331a95876a21fd94e21ce54b366dc721
finnolin <finnolin@gmail.com>
2021-04-02 01:41:44 +0200

    added VoiceLive Touch 2

--- miho.py
+++ miho.py
@@ -6,7 +6,7 @@ import subprocess
 #Arturia KeyStep 32
 #reface CS
 #Circuit
-
+#VoiceLive Touch 2
 
 
 #CONNECTIONS:
@@ -15,9 +15,12 @@ import subprocess
 subprocess.Popen(["aconnect", "OP-Z", "OP-1 Midi Device"])
 subprocess.Popen(["aconnect", "OP-Z", "reface CS"])
 subprocess.Popen(["aconnect", "OP-Z", "Circuit"])
+subprocess.Popen(["aconnect", "OP-Z", "VoiceLive Touch 2"])
+
 
 #Keystep Out
 subprocess.Popen(["aconnect", "Arturia KeyStep 32", "OP-1 Midi Device"])
 subprocess.Popen(["aconnect", "Arturia KeyStep 32", "OP-Z"])
 subprocess.Popen(["aconnect", "Arturia KeyStep 32", "reface CS"])
 subprocess.Popen(["aconnect", "Arturia KeyStep 32", "Circuit"])
+subprocess.Popen(["aconnect", "Arturia KeyStep 32", "VoiceLive Touch 2"])
