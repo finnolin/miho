d64e001cbda87dcc620613c6b60fcd735e8196d1
finnolin <finnolin@gmail.com>
2021-04-01 21:01:42 +0200

    added Circuit

--- miho.py
+++ miho.py
@@ -5,6 +5,7 @@ import subprocess
 #OP-1 Midi Device
 #Arturia KeyStep 32
 #reface CS
+#Circuit
 
 
 
@@ -13,9 +14,10 @@ import subprocess
 #OP-Z Out
 subprocess.Popen(["aconnect", "OP-Z", "OP-1 Midi Device"])
 subprocess.Popen(["aconnect", "OP-Z", "reface CS"])
-
+subprocess.Popen(["aconnect", "OP-Z", "Circuit"])
 
 #Keystep Out
 subprocess.Popen(["aconnect", "Arturia KeyStep 32", "OP-1 Midi Device"])
 subprocess.Popen(["aconnect", "Arturia KeyStep 32", "OP-Z"])
 subprocess.Popen(["aconnect", "Arturia KeyStep 32", "reface CS"])
+subprocess.Popen(["aconnect", "Arturia KeyStep 32", "Circuit"])
