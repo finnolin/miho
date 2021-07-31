874925c7e9c620db646457485e54d998bc3a76ae
finnolin <finnolin@gmail.com>
2021-07-31 11:31:30 +0200

    updated OP-1 device name

--- miho.py
+++ miho.py
@@ -2,7 +2,7 @@ import subprocess
 
 #-------DEVICES-------
 #OP-Z
-#OP-1 Midi Device
+#OP-1
 #Arturia KeyStep 32
 #reface CS
 #Circuit
@@ -13,14 +13,14 @@ import subprocess
 #CONNECTIONS:
 
 #Deluge Out
-subprocess.Popen(["aconnect", "Deluge", "OP-1 Midi Device"])
+subprocess.Popen(["aconnect", "Deluge", "OP-1"])
 subprocess.Popen(["aconnect", "Deluge", "reface CS"])
 subprocess.Popen(["aconnect", "Deluge", "VoiceLive Touch 2"])
 subprocess.Popen(["aconnect", "Deluge", "OP-Z"])
 
 
 #Keystep Out
-subprocess.Popen(["aconnect", "Arturia KeyStep 32", "OP-1 Midi Device"])
+subprocess.Popen(["aconnect", "Arturia KeyStep 32", "OP-1"])
 subprocess.Popen(["aconnect", "Arturia KeyStep 32", "OP-Z"])
 subprocess.Popen(["aconnect", "Arturia KeyStep 32", "reface CS"])
 subprocess.Popen(["aconnect", "Arturia KeyStep 32", "Deluge"])
