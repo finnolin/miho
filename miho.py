a70a98b311ae81611077b318517003a29dae17dc
finnolin <finnolin@gmail.com>
2021-07-31 10:31:23 +0200

    added deluge

--- miho.py
+++ miho.py
@@ -7,20 +7,21 @@ import subprocess
 #reface CS
 #Circuit
 #VoiceLive Touch 2
+#Deluge
 
 
 #CONNECTIONS:
 
-#OP-Z Out
-subprocess.Popen(["aconnect", "OP-Z", "OP-1 Midi Device"])
-subprocess.Popen(["aconnect", "OP-Z", "reface CS"])
-subprocess.Popen(["aconnect", "OP-Z", "Circuit"])
-subprocess.Popen(["aconnect", "OP-Z", "VoiceLive Touch 2"])
+#Deluge Out
+subprocess.Popen(["aconnect", "Deluge", "OP-1 Midi Device"])
+subprocess.Popen(["aconnect", "Deluge", "reface CS"])
+subprocess.Popen(["aconnect", "Deluge", "VoiceLive Touch 2"])
+subprocess.Popen(["aconnect", "Deluge", "OP-Z"])
 
 
 #Keystep Out
 subprocess.Popen(["aconnect", "Arturia KeyStep 32", "OP-1 Midi Device"])
 subprocess.Popen(["aconnect", "Arturia KeyStep 32", "OP-Z"])
 subprocess.Popen(["aconnect", "Arturia KeyStep 32", "reface CS"])
-subprocess.Popen(["aconnect", "Arturia KeyStep 32", "Circuit"])
+subprocess.Popen(["aconnect", "Arturia KeyStep 32", "Deluge"])
 subprocess.Popen(["aconnect", "Arturia KeyStep 32", "VoiceLive Touch 2"])
