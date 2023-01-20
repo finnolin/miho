1f451a91eefb77a9bc42c9a67981356ed06c7246
finnolin <32907340+finnolin@users.noreply.github.com>
2023-01-20 16:12:33 +0100

    added micromonsta 2

--- miho.py
+++ miho.py
@@ -8,7 +8,7 @@ import subprocess
 #Circuit
 #VoiceLive Touch 2
 #Deluge
-#Midi USB-USB
+#Midi USB-USB (Norns)
 
 
 #CONNECTIONS:
@@ -19,6 +19,7 @@ subprocess.Popen(["aconnect", "Deluge", "reface CS"])
 subprocess.Popen(["aconnect", "Deluge", "VoiceLive Touch 2"])
 subprocess.Popen(["aconnect", "Deluge", "OP-Z"])
 subprocess.Popen(["aconnect", "Deluge", "MIDI USB-USB:0"])
+subprocess.Popen(["aconnect", "Deluge", "MicroMonsta 2"])
 
 #Keystep Out
 subprocess.Popen(["aconnect", "Arturia KeyStep 32", "OP-1"])
@@ -27,7 +28,9 @@ subprocess.Popen(["aconnect", "Arturia KeyStep 32", "reface CS"])
 subprocess.Popen(["aconnect", "Arturia KeyStep 32", "Deluge"])
 subprocess.Popen(["aconnect", "Arturia KeyStep 32", "VoiceLive Touch 2"])
 subprocess.Popen(["aconnect", "Arturia KeyStep 32", "MIDI USB-USB:0"])
+subprocess.Popen(["aconnect", "Arturia KeyStep 32", "MicroMonsta 2"])
 
 #Norns Out
 subprocess.Popen(["aconnect", "MIDI USB-USB:0", "OP-1"])
 subprocess.Popen(["aconnect", "MIDI USB-USB:0", "reface CS"])
+subprocess.Popen(["aconnect", "MIDI USB-USB:0", "MicroMonsta 2"])
